c47eb4e0f9c2a991e151e6a7a83c9c60d38b7c33
Moulendu Khanra <moulenduk@gmail.com>
2026-04-13 23:33:40 +0530

    changed db settings

--- config/db-connect.js
+++ config/db-connect.js
@@ -1,6 +1,14 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 
-const connect = async () => {
-  await mongoose.connect(String(process.env.MONGO_URI));
-}
-module.exports = connect;
+const connect_db = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connect_db;
